9c6e097605086f9803465b1c1e1c6a4854eacd87
Bhushan G Y <bhushan.benz@gmail.com>
2024-05-17 21:20:32 +0530

    updated deepCompare polyfill

--- deepCompare.js
+++ deepCompare.js
@@ -1,28 +1,29 @@
-function compare(obj1, obj2) {
+function isObject(obj) {
+    return typeof obj === 'object' ? true : false;
+}
+
+function deepCompare(obj1, obj2) {
     const obj1Keys = Object.keys(obj1);
+    if (obj1Keys.length !== Object.keys(obj2).length) return false;
 
-    for (const key of obj1Keys) {
-        if (
-            typeof obj1[key] !== 'object' &&
-            typeof obj2[key] !== 'object' &&
-            obj1[key] !== obj2[key]
-        )
-            return false;
+    for (let key of obj1Keys) {
+        const val1 = obj1[key];
+        const val2 = obj2[key];
 
-        if (
-            typeof obj1[key] === 'object' &&
-            typeof obj2[key] === 'object' &&
-            !compare(obj1[key], obj2[key])
-        ) {
-            return false;
-        }
+        const objectCheck = isObject(val1) && isObject(val2);
+
+        if (!objectCheck && val1 !== val2) return false;
+
+        if (objectCheck && !deepCompare(val1, val2)) return false;
     }
     return true;
 }
 
 console.log(
-    compare(
-        { a: 1, b: 2, c: { d: [1, { a: 1 }] } },
-        { a: 1, b: 2, c: { d: [1, { a: 2 }] } }
+    deepCompare(
+        { a: 1, b: [1, 2, 3], c: { d: [1, 2, 3, { e: [4, { a: 112 }] }] } },
+        { a: 1, b: [1, 2, 3], c: { d: [1, 2, 3, { e: [4, { a: 111 }] }] } }
     )
 );
+
+console.log(deepCompare({ e: [4, { a: [1, {}] }] }, { e: [4, { a: [1, {}] }] }));
