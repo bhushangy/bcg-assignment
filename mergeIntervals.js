3786d598d0af62dd6afa940a90c597c6b58edbf5
Bhushan G Y <bhushan.benz@gmail.com>
2024-05-18 17:41:55 +0530

    added merge intervals polyfill

---
+++ mergeIntervals.js
@@ -0,0 +1,31 @@
+function mergeIntervals(arr) {
+    const inputArr = arr.slice().sort((a, b) => a[0] - b[0]);
+
+    const outputArr = [inputArr[0]];
+
+    for (const [currStart, currEnd] of inputArr.slice(1)) {
+        const prevStart = outputArr.at(-1)[0];
+        const prevEnd = outputArr.at(-1)[1];
+
+        if (currStart <= prevEnd) {
+            outputArr[outputArr.length - 1][1] = Math.max(prevEnd, currEnd);
+        } else {
+            outputArr.push([currStart, currEnd]);
+        }
+    }
+
+    return outputArr;
+}
+
+console.log(
+    mergeIntervals([
+        [5, 8],
+        [2, 4],
+        [1, 3],
+        [1, 10],
+        [10, 12],
+        [6, 11],
+        [4, 4],
+        [6, 7],
+    ])
+);
