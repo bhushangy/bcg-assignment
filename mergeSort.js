0bc0f7bdaa696780bc08a97146fc6498dc75714d
Bhushan G Y <bhushan.benz@gmail.com>
2024-05-25 23:55:08 +0530

    added algo for mergesort

---
+++ mergeSort.js
@@ -0,0 +1,10 @@
+function mergeSort(arr, low, high) {
+    const mid = Math.floor((low + high) / 2);
+  
+    if (low >= high) {
+    }
+  }
+  
+  const arr = [7, 5, 10, 2, 0, 1, 4, -1, 3];
+  console.log(mergeSort(arr, 0, arr.length - 1));
+  
