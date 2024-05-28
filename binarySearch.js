8a98436d1efa105638760a070e485aff46009b34
Bhushan G Y <bhushan.benz@gmail.com>
2024-05-28 22:33:53 +0530

    binary search recursive algo

--- binarySearch.js
+++ binarySearch.js
@@ -1,18 +1,29 @@
-function binarySearch(arr, n, target) {
-  let low = 0;
-  let high = n - 1;
-  let mid;
-  while (low <= high) {
-    mid = Math.ceil((low + high) / 2);
+function binarySearch(arr, low, high, target) {
+  if (low > high) return -1; // Search space exhausted. So exit from all the functions.
 
-    if (arr[mid] === target) return mid;
+  let mid = Math.ceil((low + high) / 2);
 
-    if (target > arr[mid]) low = mid + 1;
-    else high = mid - 1;
-  }
+  if (arr[mid] === target) return mid;
 
-  return -1;
+  if (target > arr[mid]) return binarySearch(arr, mid + 1, high, target);
+  else return binarySearch(arr, low, mid - 1, target);
 }
 
-const arr = [61];
-console.log(binarySearch(arr, arr.length, 61));
+const arr = [1, 5, 8, 10, 14, 20, 50, 61];
+console.log(binarySearch(arr, 0, arr.length - 1, 61));
+
+// function binarySearch(arr, n, target) {
+//     let low = 0;
+//     let high = n - 1;
+//     let mid;
+//     while (low <= high) { If element is at end or beginning, low is equal to high, hence the condition low <= high and not low < high
+//       mid = Math.ceil((low + high) / 2);
+
+//       if (arr[mid] === target) return mid;
+
+//       if (target > arr[mid]) low = mid + 1;
+//       else high = mid - 1;
+//     }
+
+//     return -1;
+//   }
